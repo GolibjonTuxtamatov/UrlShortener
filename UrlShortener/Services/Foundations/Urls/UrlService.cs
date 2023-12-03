389882af3e9872677095837a104ccc41189de4ca
99894 <99894@DESKTOP-K351R76>
2023-12-03 10:04:53 +0500

    FOUNDATIONS: Add Url

---
+++ UrlShortener/Services/Foundations/Urls/UrlService.cs
@@ -0,0 +1,42 @@
+﻿using UrlShortener.Brokers.Storages;
+using UrlShortener.Models;
+
+namespace UrlShortener.Services.Foundations.Urls
+{
+    public class UrlService : IUrlService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public UrlService(IStorageBroker storageBroker)
+        {
+            this.storageBroker = storageBroker;
+        }
+
+        public async ValueTask<UrlDto> AddUrlsAsync(UrlDto urlDto, HttpContext context)
+        {
+            if (!Uri.TryCreate(urlDto.Url, UriKind.Absolute, out var uri))
+                Results.BadRequest("Invalid url");
+
+            var random = new Random();
+            const string chars =
+                "QWERTYUIOPASDFGHJKLZXCVBNM1234567890@qwertyuiopasdfghjklzxcvbnm";
+
+            string randomString =
+                new string(Enumerable.Repeat(chars, 8)
+                .Select(x => x[random.Next(x.Length)]).ToArray());
+
+            var storedUrl = await this.storageBroker.InsertUrlAsync(
+                new Url
+                {
+                    OrginalUrl = urlDto.Url,
+                    ShortUrl = randomString,
+                });
+
+            string result = $"{context.Request.Scheme}://{context.Request.Host}/{storedUrl.ShortUrl}";
+
+            urlDto.Url = result;
+
+            return urlDto;
+        }
+    }
+}
