01955a6eca75719e12d68c5fcd8dd169e9294e65
99894 <99894@DESKTOP-K351R76>
2023-12-03 10:34:20 +0500

    FOUNDATIONS: Retrieve All Urls

--- UrlShortener/Services/Foundations/Urls/IUrlService.cs
+++ UrlShortener/Services/Foundations/Urls/IUrlService.cs
@@ -5,5 +5,6 @@ namespace UrlShortener.Services.Foundations.Urls
     public interface IUrlService
     {
         ValueTask<UrlDto> AddUrlsAsync(UrlDto urlDto,HttpContext context);
+        IQueryable<Url> RerieveAllUrls();
     }
 }
